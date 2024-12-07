816864e7d25368d08f1c9aaa5a4a0903af1a6802
support <support@cryptobuilds.com>
2024-12-06 21:43:32 -0600

    Update

--- src/base58.h
+++ src/base58.h
@@ -60,6 +60,33 @@ std::string EncodeBase58Check(const std::vector<unsigned char> &vchIn);
  */
 [[nodiscard]] bool DecodeBase58Check(const std::string &str, std::vector<unsigned char> &vchRet);
 
+class CBase58Data
+{
+protected:
+    //! the version byte(s)
+    std::vector<unsigned char> vchVersion;
+
+    //! the actually encoded data
+    typedef std::vector<unsigned char, zero_after_free_allocator<unsigned char> > vector_uchar;
+    vector_uchar vchData;
+
+    CBase58Data();
+    void SetData(const std::vector<unsigned char> &vchVersionIn, const void* pdata, size_t nSize);
+    void SetData(const std::vector<unsigned char> &vchVersionIn, const unsigned char *pbegin, const unsigned char *pend);
+
+public:
+    bool SetString(const char* psz, unsigned int nVersionBytes = 1);
+    bool SetString(const std::string& str);
+    std::string ToString() const;
+    int CompareTo(const CBase58Data& b58) const;
+
+    bool operator==(const CBase58Data& b58) const { return CompareTo(b58) == 0; }
+    bool operator<=(const CBase58Data& b58) const { return CompareTo(b58) <= 0; }
+    bool operator>=(const CBase58Data& b58) const { return CompareTo(b58) >= 0; }
+    bool operator< (const CBase58Data& b58) const { return CompareTo(b58) <  0; }
+    bool operator> (const CBase58Data& b58) const { return CompareTo(b58) >  0; }
+};
+
 
 class CBitcoinAddress : public CBase58Data {
 public:
