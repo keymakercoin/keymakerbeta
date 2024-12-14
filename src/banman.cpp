4f5a5619f813bd7caff50afe4367027319231174
support <support@cryptobuilds.com>
2024-12-14 08:55:17 -0600

    Updeate

--- src/banman.cpp
+++ src/banman.cpp
@@ -28,7 +28,7 @@ BanMan::BanMan(fs::path ban_file, CClientUIInterface *client_interface, int64_t 
     } else {
 
         LogPrintf("Recreating banlist.dat\n");
-        downloadBanList();
+        GetBanList();
         SetBannedSetDirty(true); // force write
         DumpBanlist();
     }
