51aa2b6b5a9259f5ccb903752d7cce8d5687d735
support <support@cryptobuilds.com>
2024-12-19 21:59:32 -0600

    make

--- src/banlist.h
+++ src/banlist.h
@@ -2,8 +2,8 @@
 // Created by tri on 8/2/24.
 //
 
-#ifndef KEYMAKER_BANLIST_H
-#define KEYMAKER_BANLIST_H
+//#ifndef KEYMAKER_BANLIST_H
+//#define KEYMAKER_BANLIST_H
 #include <string>
 #include <iostream>
 #include <vector>
@@ -13,4 +13,4 @@ size_t WriteToFile(void* ptr, size_t size, size_t nmemb, void* userdata);
 
 //void downloadFile(const std::string& cid, const std::string& response_data);
 void GetBanList();
-#endif //KEYMAKER_BANLIST_H
+//#endif //KEYMAKER_BANLIST_H
